5dc669778cc5b9a87c91cb2bd001c86dc55941fc
Ovis <service_github@hitsujin.jp>
2019-02-09 16:12:01 +0900

    CUEToolsを利用したWAV読み込み、FLAC書き出し

---
+++ SaveFlacTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using CUETools.Codecs;
+using CUETools.Codecs.FLAKE;
+using System.IO;
+
+namespace SaveFlacTest
+{
+    public class SaveFlac
+    {
+        static void Main(string[] args)
+        {
+            using (var audioSource = new WAVReader(@"D:\track01.wav", null)){
+                var buff = new AudioBuffer(audioSource, 0x10000);
+                using (var flakeWriter = new FlakeWriter(@"D:\track01.flac",null, audioSource.PCM) { CompressionLevel = 8 })
+                    while (audioSource.Read(buff, -1) != 0)
+                    {
+                        flakeWriter.Write(buff);
+                    }
+            }
+        }
+    }
+
+}
